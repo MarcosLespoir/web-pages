3e532826c517d3e04c4d435b802748a09f294457
Lespoir <marcos.lespoir@gmail.com>
2022-07-26 23:18:07 -0300

    Se hizo un reproductor de musica genérico para la página

--- src/Porfolio/controls.js
+++ src/Porfolio/controls.js
@@ -3,26 +3,44 @@
 const audio = document.querySelector('#player');
 const rangeCurrentTime = document.querySelector('.currentTime');
 const rangeVolume = document.querySelector('.volume');
+const labelDuration = document.querySelector('.duration');
+const labelTime = document.querySelector('.time');
+const timeTrack = document.querySelector('.time-pista');
 const buttonPlay = document.querySelector('.play');
-
 
 var play = false;
 var click = true;
 
 // Funciones
 // play & pause
+const fnConvertTime = (time) =>{
+    const minutes = Math.floor(time/60);
+    const seconds = Math.floor(time%60);
+    if (seconds<10) {
+        return `${minutes}:0${seconds}`;
+    } else {
+        return `${minutes}:${seconds}`;
+    }
+}
+const fnDuration = () => {
+    labelDuration.textContent = `${fnConvertTime(audio.duration)}`;
+}
+const fnTime = () => {
+    labelTime.textContent = `${fnConvertTime(audio.currentTime)}`;
+}
 const fnPlay = () => {
     buttonPlay.addEventListener("click", () => {
         if (play) {
             audio.pause();
             buttonPlay.textContent = '►'; //alt + 16
+            rangeCurrentTime.value = Math.floor(100 * audio.currentTime / audio.duration);
             play = false;
         } else {
             if ((Math.floor(100 * audio.currentTime / audio.duration)) != rangeCurrentTime.value) {
                 audio.currentTime = audio.duration * rangeCurrentTime.value / 100;
             }
-            audio.play()
-            buttonPlay.textContent = '▬';
+            audio.play();
+            buttonPlay.textContent = '||';
             play = true;
         };
     })
@@ -33,38 +51,43 @@ const fnCurrentTime = () => {
         rangeCurrentTime.addEventListener('mousedown', () => {
             click = false;
         });
-        if (click)
-        rangeCurrentTime.value = 100 * audio.currentTime / audio.duration;
+            rangeCurrentTime.style.backgroundSize = `${100 * audio.currentTime / audio.duration}%`;
         rangeCurrentTime.addEventListener('mouseup', () => {
-            if ((Math.floor(100 * audio.currentTime / audio.duration)) != rangeCurrentTime.value) {
+            if ((Math.floor(100 * audio.currentTime / audio.duration)) != Math.floor(rangeCurrentTime.style.backgroundSize)) {
                 audio.currentTime = audio.duration * rangeCurrentTime.value / 100;
+                rangeCurrentTime.style.backgroundSize = `${100 * audio.currentTime / audio.duration}%`;
             }
+            audio.play();
+            play = true
             click = true;
         })
-    } else {
+    } else if (!play) {
         rangeCurrentTime.addEventListener('mouseup', () => {
             if ((Math.floor(100 * audio.currentTime / audio.duration)) != rangeCurrentTime.value) {
                 audio.currentTime = audio.duration * rangeCurrentTime.value / 100;
-            }
+            };
+            buttonPlay.textContent = '||';
         })
     };
 }
 // Vol.
 const fnVolume = () => {
-    audio.volume = (rangeVolume.value/100)**2;
+    audio.volume = (rangeVolume.value / 100) ** 2;
 }
 
 
 // Timers
-
+const timerDuration = setInterval(() => {fnDuration()}, 100);
+const timerActualTime = setInterval(() => {fnTime()}, 100);
 const timerPlayPause = setInterval(fnPlay(), 100);
-const timerCurrentTime = setInterval(() => {fnCurrentTime();}, 100);
-const timerVolume = setInterval(() => {fnVolume();}, 100);
+const timerCurrentTime = setInterval(() => { fnCurrentTime(); }, 100);
+const timerVolume = setInterval(() => { fnVolume(); }, 100);
 
 
 
 // Inicializadores
 
+fnDuration();
 timerPlayPause();
 timerCurrentTime();
 timerVolume();
